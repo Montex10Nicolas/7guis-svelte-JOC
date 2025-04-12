b9994399edb69b6dec96746d36f41f368726833a
Nicolas10Montex <montexdev@protonmail.com>
2025-04-12 21:25:41 +0200

    counter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,14 @@
   let { children } = $props();
 </script>
 
+<nav class="mt-4 ml-2">
+  <a class="border-pinky rounded-sm border bg-cyan-400 px-4 py-2 uppercase" href="home">home</a>
+  <a class="border-pinky rounded-sm border bg-cyan-400 px-4 py-2 uppercase" href="counter"
+    >counter</a
+  >
+  <a
+    class="border-pinky rounded-sm border bg-cyan-400 px-4 py-2 uppercase"
+    href="temperatureConverter">Temperature</a
+  >
+</nav>
 {@render children()}
---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let counter = $state(0);
+</script>
+
+<div class="flex gap-4 p-4">
+  <p class="w-16 border border-gray-600 px-1">{counter}</p>
+  <button class="w-16 cursor-pointer rounded-sm border border-black" onclick={() => counter++}
+    >count</button
+  >
+</div>
